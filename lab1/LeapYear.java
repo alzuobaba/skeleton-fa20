acca714fc9bcd4bfd7a6308a83700eaffebf30cb
cuirui <cuirui@sinorail.com>
2021-03-21 19:06:47 +0800

    commit lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,17 @@ public class LeapYear {
         }
     }
 
+    /** num code be divide by 400 or divice by 4 not 100
+     *
+     *
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0){
+            return true;
+        }
+        else return year % 4 == 0 && year % 100 != 0;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
