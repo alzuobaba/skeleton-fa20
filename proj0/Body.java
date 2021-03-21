889fe1141c42734226639232e083d52e863b3728
cuirui <cuirui@sinorail.com>
2021-03-21 22:07:04 +0800

    add proj0

---
+++ proj0/Body.java
@@ -0,0 +1,71 @@
+public class Body {
+//    double xxPos: Its current x position
+//    double yyPos: Its current y position
+//    double xxVel: Its current velocity in the x direction
+//    double yyVel: Its current velocity in the y direction
+//    double mass: Its mass
+//    String imgFileName: The name of the file that corresponds to
+//            the image that depicts the body
+//            (for example, jupiter.gif)
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String imgFileName;
+
+    public Body(double xP, double yP, double xV,
+                double yV, double m, String img){
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    public Body(Body b){
+        this.xxPos = b.xxPos;
+        this.yyPos = b.yyPos;
+        this.xxVel = b.xxVel;
+        this.yyVel = b.yyVel;
+        this.mass = b.mass;
+        this.imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Body b){
+        return Math.sqrt(Math.pow((this.xxPos - b.xxPos), 2) + Math.pow((this.yyPos - b.yyPos), 2));
+    }
+    public double calcForceExertedBy(Body b){
+        if (this.equals(b)){
+            return 0;
+        }
+        return 6.67E-11* this.mass * b.mass/Math.pow(this.calcDistance(b), 2);
+    }
+    public double calcForceExertedByX(Body b){
+        return (b.xxPos - this.xxPos) * this.calcForceExertedBy(b)/this.calcDistance(b);
+    }
+    public double calcForceExertedByY(Body b){
+        return (b.yyPos - this.yyPos) * this.calcForceExertedBy(b)/this.calcDistance(b);
+    }
+    public double calcNetForceExertedByX(Body[] allBodys ){
+        double sum  = 0;
+        for (int i=0; i < allBodys.length; i++){
+            if (this.equals(allBodys[i])){
+                continue;
+            }
+            sum += this.calcForceExertedByX(allBodys[i]);
+        }
+        return sum;
+    }
+    public double calcNetForceExertedByY(Body[] allBodys ){
+        double sum  = 0;
+        for (int i=0; i < allBodys.length; i++){
+            if (this.equals(allBodys[i])){
+                continue;
+            }
+            sum += this.calcForceExertedByY(allBodys[i]);
+        }
+        return sum;
+    }
+}
